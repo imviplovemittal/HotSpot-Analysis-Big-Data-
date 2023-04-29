1ec8bce9d1c0cd3d742d54f2b6f72296d923c7b2
Viplove Mittal <viplovemittal2@gmail.com>
2023-04-29 17:45:21 +0530

    Add files via upload

---
+++ src/main/scala/cse512/HotzoneUtils.scala
@@ -0,0 +1,14 @@
+package cse512
+
+object HotzoneUtils {
+
+  def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
+    // YOU NEED TO CHANGE THIS PART
+    println(queryRectangle)
+    println(pointString)
+    return true // YOU NEED TO CHANGE THIS PART
+  }
+
+  // YOU NEED TO CHANGE THIS PART
+
+}
